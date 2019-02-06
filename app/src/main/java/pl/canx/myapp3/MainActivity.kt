dd0b2478a369f6c2fdbec6a0c7fd91b2b2d34f9b
Kamil <xkamilwrobel@gmail.com>
2019-02-06 20:38:57 +0100

    Simple app

---
+++ app/src/main/java/pl/canx/myapp3/MainActivity.kt
@@ -0,0 +1,27 @@
+package pl.canx.myapp3
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXTRA_MESSAGE = "pl.canx.myapp3.EXTRA_MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
